9865be3eaf1329938084a038d857067cdcaadf41
Rodrigo Rocha <rodrocha444@gmail.com>
2023-11-16 19:56:53 -0300

    :art: After aula 16-11

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: "localhost",
-    port: 5433,
-    user: "postgres",
-    password: "admin",
-    database: "postgres",
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
   });
   await client.connect();
 
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database";
 
 export default async function status(req, res) {
   const result = await database.query("SELECT 1+1 as sum;");
