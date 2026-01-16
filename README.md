996159be108cc491c77c5267f7bc5cbf10e82ee8
Rodrigo Rocha <rodrocha444@gmail.com>
2026-01-16 15:10:39 -0300

    🔥 Clean project

--- README.md
+++
@@ -1,3 +0,0 @@
-# clone-tabnews
-
-Implementação do https://www.tabnews.com.br para o https://curso.dev
--- pages/api/v1/status/index.js
+++
@@ -1,3 +0,0 @@
-export default function status(request, response) {
-  response.status(200).json({ status: "ok" });
-}
